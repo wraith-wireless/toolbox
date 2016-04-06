2accb9a3f245a57c67ee3a499cb70503324feee2
wraith-wireless <wraith.wireless@hushmail.com>
2016-04-06 07:34:21 -0600

    v 0.0.2

---
+++ __init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+""" toolbox: utility functions used in several projects
+
+Module for utility functions that I use across projects or that I no longer use
+but don't want to delete
+
+REVISIONS:
+ 0.0.2
+  desc: includes bitmask and sync object pickling
+  includes: bits 0.0.5 brine 0.0.1
+"""
+__name__ = 'toolbox'
+__license__ = 'GPL v3.0'
+__version__ = '0.0.2'
+__date__ = 'April 2016'
+__author__ = 'Dale Patterson'
+__maintainer__ = 'Dale Patterson'
+__email__ = 'wraith.wireless@yandex.com'
+__status__ = 'Production'
